593582d6319c3148b1e304450fdd9d9979ade692
TGY-forture <1981472184@qq.com>
2020-11-29 20:50:07 +0800

    更新字体图标

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 <style lang="scss">
 @import url("../src/assets/css/index.scss");
-@import url("//at.alicdn.com/t/font_2000699_h4hfhlj66f.css");
+@import url("//at.alicdn.com/t/font_2000699_1qu1ppf9qh8.css");
 #app {
   width: 1000px;
   height: 90%;
