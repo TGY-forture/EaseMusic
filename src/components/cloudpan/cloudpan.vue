ade13f026b55118a786daeed0eca0b8757364c6f
TGY-forture <1981472184@qq.com>
2020-12-04 23:01:07 +0800

    添加组件

---
+++ src/components/cloudpan/cloudpan.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>xxxxxxxxx</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/video/video.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="video">djkfjkdfjkdjf</div>
+</template>
+
+<script>
+export default {
+  name: "films",
+};
+</script>
+
+<style lang="scss">
+.video {
+  height: calc(100% - 56px);
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+  &::-webkit-scrollbar-track {
+    background-color: white;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #b2bec3;
+    border-radius: 2px;
+  }
+}
+</style>
