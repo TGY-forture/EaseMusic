41303f5da62f10718108d8a2b042b77afdadec7a
TGY-forture <1981472184@qq.com>
2020-12-04 23:02:19 +0800

    路由添加

--- src/views/left.vue
+++ src/views/left.vue
@@ -9,32 +9,46 @@
     <section class="online-m">
       <p>在线音乐</p>
       <ul>
-        <li v-for="(item, index) in onlinelist" :key="index">
+        <router-link
+          :to="item.path"
+          tag="li"
+          v-for="(item, index) in onlinelist"
+          :key="index"
+          exact
+          exact-active-class="top-active"
+        >
           <i :class="['custom-icon', item.classname]"></i>
           <span>{{ item.text }}</span>
-        </li>
+        </router-link>
       </ul>
     </section>
     <section class="mymusic">
       <p>我的音乐</p>
       <ul>
-        <li v-for="(item, index) in mymusic" :key="index">
+        <router-link
+          :to="item.path"
+          tag="li"
+          v-for="(item, index) in mymusic"
+          :key="index"
+          exact
+          exact-active-class="top-active"
+        >
           <i :class="['custom-icon', item.classname]"></i>
           <span>{{ item.text }}</span>
-        </li>
+        </router-link>
       </ul>
     </section>
     <section class="mylist">
       <p>我的歌单</p>
       <ul>
-        <li>
+        <router-link to="/like" tag="li" exact exact-active-class="top-active">
           <i class="custom-icon custom-icon-heart-copy-copy"></i>
           <span>我喜欢</span>
           <i
             class="custom-icon custom-icon-heart"
             style="margin-left: auto"
           ></i>
-        </li>
+        </router-link>
       </ul>
     </section>
   </div>
@@ -45,32 +59,34 @@ let onlinelist = [
   {
     text: "音乐馆",
     classname: "custom-icon-music",
+    path: "/musicroom",
   },
   {
     text: "视频",
     classname: "custom-icon-video",
+    path: "/films",
   },
   {
     text: "电台",
     classname: "custom-icon-radio",
+    path: "/musicroom/dj",
   },
 ];
 let mymusic = [
   {
-    text: "试听列表",
-    classname: "custom-icon-28-Navigationfrieslistmenuoptionsstack",
-  },
-  {
     text: "播放历史",
     classname: "custom-icon-history",
+    path: "/history",
   },
   {
     text: "音乐云盘",
     classname: "custom-icon-Ioniconsmdcloudoutline",
+    path: "/cloudpan",
   },
   {
     text: "我的收藏",
     classname: "custom-icon-user-start",
+    path: "/collection",
   },
 ];
 
@@ -91,7 +107,6 @@ export default {
   width: 190px;
   height: 100%;
   background-color: #f0f0f0;
-
   box-sizing: border-box;
   header {
     display: flex;
@@ -125,10 +140,10 @@ export default {
       padding-bottom: 5px;
       display: flex;
       align-items: center;
+      border-radius: 6px;
       cursor: pointer;
       &:hover {
         background-color: #d9d9d9;
-        border-radius: 6px;
       }
       span {
         font-size: 14px;
@@ -139,6 +154,9 @@ export default {
         font-size: 18px;
       }
     }
+  }
+  .top-active {
+    background-color: #d9d9d9;
   }
 }
 </style>
