fb57f71b5628f5c1baf2bd80b27d66b626d2add6
TGY-forture <1981472184@qq.com>
2020-12-05 21:46:45 +0800

    添加视频页面

---
+++ src/api/films/video.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+async function getVideoLabel() {
+  const { data: { data } } = await axios.get('http://localhost:3000/video/group/list')
+  return data.map(item => {
+    return {
+      name: item.name,
+      id: item.id
+    }
+  })
+}
+
+async function getVideoKind() {
+  const { data: { data } } = await axios.get('http://localhost:3000/video/category/list')
+  return data.map(item => {
+    return {
+      name: item.name,
+      id: item.id
+    }
+  })
+}
+
+async function getVideoLists(id, offset = 0) {
+  const { data: { datas } } = await axios.get(`http://localhost:3000/video/group?id=${id}&offset=${offset}`)
+  return datas.map(({ data }) => {
+    return {
+      coverUrl: data.coverUrl,
+      title: data.title,
+      nickname: data.creator.nickname,
+      previewUrl: data.previewUrl,
+      durationms: data.durationms,
+      playTime: data.playTime,
+      vid: data.vid,
+      width: data.width,
+      height: data.height
+    }
+  })
+}
+
+
+export {
+  getVideoLabel, getVideoKind, getVideoLists
+}
