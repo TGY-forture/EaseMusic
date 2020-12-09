13fb9af6474daba4590dd1fe8c5caa294fc7a348
TGY-forture <1981472184@qq.com>
2020-12-09 23:25:01 +0800

    使用vuex保存歌曲数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import { getMp3Url, getLikes, getSongDetail } from '@/api/like/like.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    playlist: [],
+  },
+  getters: {
+    len(state) {
+      return state.playlist.length
+    }
   },
   mutations: {
+    initPlaylist(state, sinfo) {
+      state.playlist = sinfo
+    }
   },
   actions: {
+    async getLoveMusic({ commit }, uid) {
+      const ids = await getLikes()
+      const songs = await getSongDetail(ids.slice(0, 50).join(","))
+      commit('initPlaylist', songs)
+    }
   },
   modules: {
   }
+  , strict: true
 })
